198581bd4ebe07594a3d4426d9872dc4575c949f
RArielAlonso <rariel.alonso@gmail.com>
2024-01-20 17:15:53 -0300

    agregado de database

---
+++ ingestion/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from time import time
+
+conn_str='postgresql://aalonso:aalonso@postgres-db:5432/database_nyc'
+engine=create_engine(conn_str)
+df_iter=pd.read_csv("/tmp/yellow_tripdata_2021-01.csv",iterator=True,chunksize=100000)
+
+while True:
+    t_start_time=time()
+    df=next(df_iter)
+    df.tpep_pickup_datetime=pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime=pd.to_datetime(df.tpep_dropoff_datetime)
+    df.to_sql(name='nyc_table', con=engine, if_exists='append', index=False)
+    t_end_time=time()
+    print("Inserting df it took %.3f second" %(t_end_time-t_start_time))
