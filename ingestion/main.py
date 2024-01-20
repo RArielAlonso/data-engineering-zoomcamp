53f2a94677c97cc8764e2749153ea97b1d0663fd
RArielAlonso <rariel.alonso@gmail.com>
2024-01-20 20:33:54 -0300

    finished data ingestion and tables for excersice 1

--- ingestion/main.py
+++ ingestion/main.py
@@ -4,13 +4,26 @@ from time import time
 
 conn_str='postgresql://aalonso:aalonso@postgres-db:5432/database_nyc'
 engine=create_engine(conn_str)
-df_iter=pd.read_csv("/tmp/yellow_tripdata_2021-01.csv",iterator=True,chunksize=100000)
+df_iter1=pd.read_csv("/tmp/green_tripdata_2019-09.csv",iterator=True,chunksize=100000)
+df_zone=pd.read_csv("/tmp/taxi_zone_lookup.csv",iterator=True,chunksize=100000)
 
-while True:
-    t_start_time=time()
-    df=next(df_iter)
-    df.tpep_pickup_datetime=pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime=pd.to_datetime(df.tpep_dropoff_datetime)
-    df.to_sql(name='nyc_table', con=engine, if_exists='append', index=False)
-    t_end_time=time()
-    print("Inserting df it took %.3f second" %(t_end_time-t_start_time))
+
+def main():
+   while True:
+    try:
+        t_start_time=time()
+        df=next(df_iter1)
+        df['lpep_pickup_datetime']=pd.to_datetime(df['lpep_pickup_datetime'])
+        df['lpep_dropoff_datetime']=pd.to_datetime(df['lpep_dropoff_datetime'])
+        df.to_sql(name='nyc_table', con=engine, if_exists='append', index=False)
+        t_end_time=time()
+        print("Inserting df it took %.3f second" %(t_end_time-t_start_time))
+        df_zone=pd.read_csv("/tmp/taxi_zone_lookup.csv")
+        df_zone.to_sql(name='zones_taxi', con=engine, if_exists='replace', index=False)
+    
+    except StopIteration:
+       print("Finished the loading data to postgres")
+       break
+
+if __name__ == "__main__":
+    main()
